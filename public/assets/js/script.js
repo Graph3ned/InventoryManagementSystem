608070bb6edcc1ac30574973e4c7f8822926a93f
Ariel Noja <amnoja@nemsu.edu.ph>
2025-09-14 23:03:09 +0800

    ako gi add ang asset ni edd

---
+++ public/assets/js/script.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector(".wrapper");
+const registerLink = document.querySelector(".register-link");
+const loginLink = document.querySelector(".login-link");
+const loginPopupBtn = document.querySelector(".login-pop");
+const iconClose = document.querySelector(".icon-close");
+
+// Show login/register popup
+loginPopupBtn.onclick = () => {
+    wrapper.classList.add("active-popup");
+};
+
+// Hide popup
+iconClose.onclick = () => {
+    wrapper.classList.remove("active-popup");
+    wrapper.classList.remove("active"); // reset to login
+};
+
+// Switch to register form
+// registerLink.onclick = () => {
+//     wrapper.classList.add("active");
+// };
+
+// Switch back to login form
+loginLink.onclick = () => {
+    wrapper.classList.remove("active");
+};
